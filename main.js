56619a05ef565dd706a136b5f067017e50a03588
Leonardo Cabezas Grajales <numeraljoker@gmail.cpm>
2022-09-06 23:28:48 -0500

    se introdujo ficha descripcion de producto

--- main.js
+++ main.js
@@ -19,7 +19,7 @@ function toggleDesktopMenu() {
   if (!isAsideClosed) {
     shoppingCartContainer.classList.add('inactive');
   }
-  
+
   desktopMenu.classList.toggle('inactive');
 }
 
@@ -27,27 +27,27 @@ function toggleMobileMenu() {
   const isAsideClosed = shoppingCartContainer.classList.contains('inactive');
 
   if (!isAsideClosed) {
-    shoppingCartContainer.classList.add('inactive'); 
+    shoppingCartContainer.classList.add('inactive');
   }
 
   closeProductDetailAside();
-  
+
   mobileMenu.classList.toggle('inactive');
 }
 
 function toggleCarritoAside() {
   const isMobileMenuClosed = mobileMenu.classList.contains('inactive');
-  
+
   if (!isMobileMenuClosed) {
     mobileMenu.classList.add('inactive');
   }
 
   const isProductDetailClosed = productDetailContainer.classList.contains('inactive');
-  
+
   if (!isProductDetailClosed) {
-    productDetailContainer.classList.add('inactive'); 
+    productDetailContainer.classList.add('inactive');
   }
-  
+
   shoppingCartContainer.classList.toggle('inactive');
 }
 
@@ -81,37 +81,37 @@ function renderProducts(arr) {
   for (product of arr) {
     const productCard = document.createElement('div');
     productCard.classList.add('product-card');
-  
+
     // product= {name, price, image} -> product.image
     const productImg = document.createElement('img');
     productImg.setAttribute('src', product.image);
     productImg.addEventListener('click', openProductDetailAside);
-  
+
     const productInfo = document.createElement('div');
     productInfo.classList.add('product-info');
-  
+
     const productInfoDiv = document.createElement('div');
-  
+
     const productPrice = document.createElement('p');
     productPrice.innerText = '$' + product.price;
     const productName = document.createElement('p');
     productName.innerText = product.name;
-  
+
     productInfoDiv.appendChild(productPrice);
     productInfoDiv.appendChild(productName);
-  
+
     const productInfoFigure = document.createElement('figure');
     const productImgCart = document.createElement('img');
     productImgCart.setAttribute('src', './icons/bt_add_to_cart.svg');
-  
+
     productInfoFigure.appendChild(productImgCart);
-  
+
     productInfo.appendChild(productInfoDiv);
     productInfo.appendChild(productInfoFigure);
-  
+
     productCard.appendChild(productImg);
     productCard.appendChild(productInfo);
-  
+
     cardsContainer.appendChild(productCard);
   }
 }
